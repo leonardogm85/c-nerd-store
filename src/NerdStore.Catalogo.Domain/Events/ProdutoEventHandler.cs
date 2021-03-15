da7db17292774ce960995e4eca29b2865b53e05a
leonardo.martins <leonardo.martins@squadra.com.br>
2021-03-14 22:50:46 -0300

    Lançando eventos de domínio

---
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent mensagem, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.ObterPorId(mensagem.AggregateId);
+
+            // TODO: Enviar um email para aquisição de mais produtos.
+        }
+    }
+}
